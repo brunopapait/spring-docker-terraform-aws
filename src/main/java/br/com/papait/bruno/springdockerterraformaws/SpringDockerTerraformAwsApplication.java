7a8b9b373d57010918ca67bfe281b76d62b1dcc8
brunopapait <brunohpapait@gmail.com>
2023-04-26 20:52:41 -0300

    first commit

---
+++ src/main/java/br/com/papait/bruno/springdockerterraformaws/SpringDockerTerraformAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.papait.bruno.springdockerterraformaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerTerraformAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerTerraformAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/papait/bruno/springdockerterraformaws/SpringDockerTerraformAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.papait.bruno.springdockerterraformaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerTerraformAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
